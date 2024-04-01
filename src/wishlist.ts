bdfeac0c19fba9f9196717d148ddbc2eed000eca
Evan Summers <tofuevan@gmail.com>
2024-04-01 20:58:56 +0200

    More suggestions

--- src/wishlist.ts
+++ src/wishlist.ts
@@ -9,6 +9,7 @@ export type PurchasableItem = {
 
 export const wishlist: PurchasableItem[] = [
   { for: 'Tug', cost: 50, level: 3, name: "Crushing (Weapon Property Rune)", link: "https://2e.aonprd.com/Equipment.aspx?ID=1298", desc: "When you critically hit a target with this weapon, your target becomes clumsy 1 and enfeebled 1 until the end of your next turn" },
+  { for: 'Tug', cost: 58, level: 3, name: "Bracelet of Dashing", link: "https://2e.aonprd.com/Equipment.aspx?ID=415", desc: "Once per day, gain a +10-foot status bonus to your Speed for 1 minute" },
   { for: 'Tug', cost: 65, level: 4, name: "Striking (Weapon Fundamental Rune)", link: "https://2e.aonprd.com/Equipment.aspx?ID=280", desc: "Increases weapon damage dice (e.g. 1d10 => 2d10)" },
   { for: 'Tug', cost: 85, level: 4, name: "Belt of Good Health", link: "https://2e.aonprd.com/Equipment.aspx?ID=913", desc: "+4 extra HP" },
   { for: 'Tug', cost: 80, level: 4, name: "Healer's Gloves", link: "https://2e.aonprd.com/Equipment.aspx?ID=444", desc: "Action to restore 2d6+7 HP to an adjacent creature once per day, +1 item bonus to Medicine checks" },
@@ -31,6 +32,8 @@ export const wishlist: PurchasableItem[] = [
   { for: 'Twotail', cost: 24000, level: 18, name: "Mercurial Mantle", link: "https://2e.aonprd.com/Equipment.aspx?ID=1067", desc: "+2 to Dexterity, other powerful roguish abilities" },
 
   { for: 'Merrek', cost: 52, level: 3, name: "Bracers of Missile Deflection", link: "https://2e.aonprd.com/Equipment.aspx?ID=3061", desc: "Once-per-day reaction to gain +2 AC against a non-crit ranged weapon attack" },
+  { for: 'Merrek', cost: 58, level: 3, name: "Bracelet of Dashing", link: "https://2e.aonprd.com/Equipment.aspx?ID=415", desc: "Once per day, gain a +10-foot status bonus to your Speed for 1 minute" },
+  { for: 'Merrek', cost: 80, level: 4, name: "Lifting Belt", link: "https://2e.aonprd.com/Equipment.aspx?ID=3091&Redirected=1", desc: "+1 item bonus to Athletics, extra carrying capacity, magic lift action" },
   { for: 'Merrek', cost: 80, level: 4, name: "Healer's Gloves", link: "https://2e.aonprd.com/Equipment.aspx?ID=444", desc: "Action to restore 2d6+7 HP to an adjacent creature once per day, +1 item bonus to Medicine checks" },
   { for: 'Merrek', cost: 85, level: 4, name: "Belt of Good Health", link: "https://2e.aonprd.com/Equipment.aspx?ID=913", desc: "+4 extra HP" },
   { for: 'Merrek', cost: 100, level: 4, name: "Sleeves of Storage", link: "https://2e.aonprd.com/Equipment.aspx?ID=745", desc: "Worn garment which magically holds up to 10 Bulk with reduced weight" },
@@ -51,6 +54,7 @@ export const wishlist: PurchasableItem[] = [
   { for: 'Crimson', cost: 85, level: 4, name: "Belt of Good Health", link: "https://2e.aonprd.com/Equipment.aspx?ID=913", desc: "+4 extra HP" },
   { for: 'Crimson', cost: 100, level: 4, name: "Sleeves of Storage", link: "https://2e.aonprd.com/Equipment.aspx?ID=745", desc: "Worn garment which magically holds up to 10 Bulk with reduced weight" },
   { for: 'Crimson', cost: 340, level: 8, name: "Resilient (Armor Fundamental Rune)", link: "https://2e.aonprd.com/Equipment.aspx?ID=2786", desc: "+1 to all saving throws" },
+  { for: 'Crimson', cost: 470, level: 8, name: "Staff of Healing (Greater)", link: "https://2e.aonprd.com/Equipment.aspx?ID=3042", desc: "+2 item bonus to all healing, extra heal spells, other spells" },
   { for: 'Crimson', cost: 1000, level: 10, name: "Shadow Signet", link: "https://2e.aonprd.com/Equipment.aspx?ID=3108&NoRedirect=1", desc: "Free action metamagic which changes spell to target Fortitude or Reflex instead of AC" },
   { for: 'Crimson', cost: 700, level: 9, name: "Healer's Gloves (Greater)", link: "https://2e.aonprd.com/Equipment.aspx?ID=444", desc: "Action to restore 4d6+15 HP to an adjacent creature once per day, +2 item bonus to Medicine checks" },
 
